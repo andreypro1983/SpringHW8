5b34c1228c23e36a07c6313ab9a97f45d6b8c94f
Prozhikin_Andrey <a.prozhikin@gmail.com>
2024-02-27 23:48:48 +0200

    rename folder test1

--- src/test/java/ru/geekbrains/SpringHW8/SpringHw8ApplicationTests.java
+++ src/test/java/ru/geekbrains/SpringHW8/SpringHw8ApplicationTests.java
@@ -1,10 +1,10 @@
-package ru.geekbrains.SpringHW5;
+package ru.geekbrains.SpringHW8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringHw5ApplicationTests {
+class SpringHw8ApplicationTests {
 
 	@Test
 	void contextLoads() {
